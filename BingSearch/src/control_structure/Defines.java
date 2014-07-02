c55e6a0ed0cc3499ebee962db889a7261cc931a2
Mikhail Andreev <mikhail.andreev@pemicro.com>
2014-07-02 12:01:35 -0400

    Added logging to BingSeach

--- BingSearch/src/control_structure/Defines.java
+++ BingSearch/src/control_structure/Defines.java
@@ -14,4 +14,7 @@ public class Defines {
 	public static final int DELAY_GO = 5000;
 	public static final int DELAY_SEARCH = 30000;
 	public static final int NUMBER_OF_SEARCHES = 90;
+	public static final int LOGGING_LEVEL = 5;
+	public static final String LOG_FILE_LOCATION = "log.txt";
+	public static final String VERSION = "1.0";
 }
