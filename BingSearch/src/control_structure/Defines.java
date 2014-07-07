06dd2ba5d1d294e8729a05ef0ed5ffcf3951f346
Mikhail Andreev <mikha.andreev@gmail.com>
2014-07-06 20:57:05 -0400

    Small change to Defines

--- BingSearch/src/control_structure/Defines.java
+++ BingSearch/src/control_structure/Defines.java
@@ -16,5 +16,5 @@ public class Defines {
 	public static final int NUMBER_OF_SEARCHES = 90;
 	public static final int LOGGING_LEVEL = 5;
 	public static final String LOG_FILE_LOCATION = "log.txt";
-	public static final String VERSION = "1.0";
+	public static final String VERSION = "1.1";
 }
